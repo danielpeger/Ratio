714da5e9815edae018d0c7b25196d0cac5afee48
Daniel Peger <pegerdaniel@gmail.com>
2025-07-29 20:48:54 +0200

    brews list

---
+++ Ratio/Components/BrewRowView.swift
@@ -0,0 +1,18 @@
+//
+//  BrewRowView.swift
+//  Ratio
+//
+//  Created by Daniel Péger on 2025. 07. 29..
+//
+
+import SwiftUI
+
+struct BrewRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BrewRowView()
+}
